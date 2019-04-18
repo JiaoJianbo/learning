a4fff46709f35cec0486427dae6386d6294afa34
Bobby Jiao <jiaojb84@gmail.com>
2019-04-18 17:50:12 +0800

    Add i18n support.

--- spring-mybatis/src/main/java/com/bravo/demo/ssm/config/WebMvcConfig.java
+++ spring-mybatis/src/main/java/com/bravo/demo/ssm/config/WebMvcConfig.java
@@ -2,13 +2,20 @@ package com.bravo.demo.ssm.config;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.util.WebUtils;
 
 import com.bravo.demo.ssm.filter.TimerLoggerFilter;
 import com.bravo.demo.ssm.interceptor.TimerLoggerInterceptor;
@@ -49,58 +56,56 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 	
 	
 	
-	
-	
-//	/**
-//	 * 这个 Bean 是为了支持自由切换语言。
-//	 * 即使没这个 Bean，一旦配置了spring.messages.basename， Spring Boot 也会根据当前的系统选择默认语言
-//	 */
-//    @Bean
-//    public LocaleResolver localeResolver() {
-//        return new MyLocaleResolver();
-//    }
+
 //    
 //	@Override
 //	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 //		registry.addResourceHandler("swagger-ui.html")
 //				.addResourceLocations("classpath:/META-INF/resources/");
 //
-//	}
+//	}	
+	
+	/**
+	 * 这个 Bean 是为了支持自由切换语言。
+	 * 即使没这个 Bean，一旦配置了spring.messages.basename， Spring Boot 也会根据当前的系统选择默认语言
+	 */
+	@Bean
+	public LocaleResolver localeResolver() {
+		return new MyLocaleResolver();
+	}
 }
 
-//class MyLocaleResolver implements LocaleResolver {
-//	private static final String LANGUAGE = "lang";
-//	private static final String LANGUAGE_SESSION = "lang_session";
-//
-//	@Override
-//	public Locale resolveLocale(HttpServletRequest request) {
-//		Map<String, String[]> map = request.getParameterMap();
-//		String language = request.getParameter(LANGUAGE);
-//		String language2 = request.getParameter("language");
-//		Locale locale = Locale.getDefault();
-//		
-//		if(!StringUtils.isEmpty(language)) {
-//			String[] strs = language.split("_");
-//			locale = new Locale(strs[0], strs[1]);
-//			
-//			// TODO 是否应该记住所选语言，或记录到session？
-//		//将国际化语言保存到session
-//		//HttpSession session = request.getSession();
-//		//session.setAttribute(LANGUAGE_SESSION, locale);
-//		} else {
-//			// TODO 如没有，是否取之前保存的或者session里的配置？
-//			//HttpSession session = request.getSession();
-//			//Locale localeInSession = (Locale) session.getAttribute(LANGUAGE_SESSION);
-//			//if(localeInSession != null) {
-//			//	locale = localeInSession;
-//			//}
-//		}
-//		
-//		return locale;
-//	}
-//
-//	@Override
-//	public void setLocale(HttpServletRequest request, HttpServletResponse response, Locale locale) {
-//	}
-//	
-//}
+class MyLocaleResolver implements LocaleResolver {
+	private static final String LANGUAGE = "lang";
+	private static final String LANGUAGE_SESSION = "lang_session";
+
+	@Override
+	public Locale resolveLocale(HttpServletRequest request) {
+		// 如果前后端分离，每次请求默认还要带上这个参数
+		String language = request.getParameter(LANGUAGE);
+		if(StringUtils.isBlank(language)) {
+			language = request.getHeader(LANGUAGE); // 如果从参数中取不到，再尝试从header中取
+		}
+		Locale locale = Locale.getDefault();
+		
+		if(!StringUtils.isEmpty(language)) {
+			String[] strs = language.split("_");
+			locale = new Locale(strs[0], strs[1]);
+			
+			//将国际化语言保存到session
+			WebUtils.setSessionAttribute(request, LANGUAGE_SESSION, locale);
+		} else {
+			// 如没有，取之前保存的或者session里的配置
+			Locale localeInSession = (Locale)WebUtils.getSessionAttribute(request, LANGUAGE_SESSION);
+			if(localeInSession != null) {
+				locale = localeInSession;
+			}
+		}
+		
+		return locale;
+	}
+
+	@Override
+	public void setLocale(HttpServletRequest request, HttpServletResponse response, Locale locale) {
+	}
+}
