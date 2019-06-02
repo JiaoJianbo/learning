0e144ce4d1d35275f3943eb1a7097c292baa018f
Bobby Jiao <jiaojb84@gmail.com>
2019-06-02 23:12:00 +0800

    Add more cases.

---
+++ interview/src/main/java/com/bravo/interview/juc/SpinLockDemo.java
@@ -0,0 +1,44 @@
+package com.bravo.interview.juc;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * 自旋锁示例
+ * @author Bobby
+ */
+public class SpinLockDemo {
+    public static void main(String[] args) {
+        SpinLock lock = new SpinLock();
+
+        new Thread(() -> {
+            lock.lock();
+            try{TimeUnit.SECONDS.sleep(3);} catch (InterruptedException e) {e.printStackTrace();}
+            lock.unlock();
+        }, "AAA").start();
+
+        new Thread(() -> {
+            lock.lock();
+            try{TimeUnit.SECONDS.sleep(1);} catch (InterruptedException e) {e.printStackTrace();}
+            lock.unlock();
+        }, "BBB").start();
+
+    }
+}
+
+class SpinLock {
+    AtomicReference<Thread> reference = new AtomicReference();
+
+    public void lock() {
+        Thread thread = Thread.currentThread();
+        System.out.println(thread.getName() + " coming in");
+        while(!reference.compareAndSet(null, thread)){
+        }
+    }
+
+    public void unlock() {
+        Thread thread = Thread.currentThread();
+        reference.compareAndSet(thread, null);
+        System.out.println(thread.getName() + " exit.");
+    }
+}
