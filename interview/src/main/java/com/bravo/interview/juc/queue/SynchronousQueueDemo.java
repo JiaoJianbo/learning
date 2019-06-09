b25ea710e49607351a389a02fc9204c835d7c2e6
Bobby Jiao <jiaojb84@gmail.com>
2019-06-09 23:28:43 +0800

    Add more cases.' | git push

--- interview/src/main/java/com/bravo/interview/juc/queue/SynchronousQueueDemo.java
+++ interview/src/main/java/com/bravo/interview/juc/queue/SynchronousQueueDemo.java
@@ -1,6 +1,7 @@
 package com.bravo.interview.juc.queue;
 
 import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Bobby
@@ -9,5 +10,29 @@ import java.util.concurrent.SynchronousQueue;
 public class SynchronousQueueDemo {
     public static void main(String[] args) {
         SynchronousQueue synchronousQueue = new SynchronousQueue();
+
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    synchronousQueue.put(i);
+                    System.out.println(Thread.currentThread().getName() + " put " + i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "AAA").start();
+
+
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(500);
+                    Object o = synchronousQueue.take();
+                    System.out.println(Thread.currentThread().getName() + " take " + o);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "BBB").start();
     }
 }
