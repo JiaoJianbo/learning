ddcf79ae2f5b7851843b8242151097a578dae756
Bobby Jiao <jiaojb84@gmail.com>
2019-11-10 23:46:00 +0800

    Enable HTTPS

--- springboot-demo3/README.md
+++ springboot-demo3/README.md
@@ -85,3 +85,48 @@
     [JSONAssert – How to unit test JSON data](https://www.mkyong.com/java/jsonassert-how-to-unit-test-json-data/)  
     [Spring REST Integration Test Example](https://www.mkyong.com/spring-boot/spring-rest-integration-test-example/)  
     [JSONassert 1.5.1-SNAPSHOT API](http://jsonassert.skyscreamer.org/apidocs/index.html)
+    
+4. 启用 HTTPS 访问方式。
+    
+    1). 使用命令`keytool -genkey -alias mytomcat -keyalg RSA -keystore D:\keystore.jks -keysize 2048`生成证书。
+
+    ```text
+    C:\Users\Bobby>keytool -genkey -alias mytomcat -keyalg RSA -keystore D:\keystore.jks -keysize 2048
+    输入密钥库口令:
+    再次输入新口令:
+    您的名字与姓氏是什么?
+      [Unknown]:
+    您的组织单位名称是什么?
+      [Unknown]:
+    您的组织名称是什么?
+      [Unknown]:
+    您所在的城市或区域名称是什么?
+      [Unknown]:
+    您所在的省/市/自治区名称是什么?
+      [Unknown]:
+    该单位的双字母国家/地区代码是什么?
+      [Unknown]:
+    CN=Unknown, OU=Unknown, O=Unknown, L=Unknown, ST=Unknown, C=Unknown是否正确?
+      [否]:  Y
+    
+    输入 <mytomcat> 的密钥口令
+            (如果和密钥库口令相同, 按回车):
+    
+    Warning:
+    JKS 密钥库使用专用格式。建议使用 "keytool -importkeystore -srckeystore D:\keystore.jks -destkeystore D:\keystore.jks -deststoretype pkcs12" 迁移到行业标准格式 PKCS12。
+    ```
+    
+    2). 拷贝证书到 resource 目录下，并配置 application.yml。这里的 password 就是刚才生成证书时输入的。
+    
+    ```yaml
+    server:
+      port: 8443
+      ssl:
+        key-store: classpath:keystore.jks
+        key-password: my@secret
+        key-store-password: my@secret
+    ```
+    
+    **参考:**  
+    [常用的Java Keytool Keystore命令](https://csr.chinassl.net/keytool-commands.html)
+    
---
+++ springboot-demo3/src/main/java/com/bravo/demo/springbootdemo3/config/TomcatConfig.java
@@ -0,0 +1,54 @@
+package com.bravo.demo.springbootdemo3.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 为了配置 HTTPS 而添加的配置。将用户发起额 HTTP 请求，自动转发到 HTTPS 上。
+ *
+ * @author Bobby
+ * @since 2019/11/10 23:22
+ */
+@Configuration
+public class TomcatConfig {
+    /**
+     * 配置了 HTTP 的请求端口为 8080，所有来自 8080 的请求，将被自动重定向到 8443 这个 HTTPS 的端口上。
+     * @return
+     */
+    @Bean
+    public Connector connector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+
+        return connector;
+    }
+
+    /*
+     * 也有说用 EmbeddedServletContainerFactory 或 TomcatEmbeddedServletContainerFactory，
+     * 但是在 Spring Boot 2.x 中要用 TomcatServletWebServerFactory
+     */
+    @Bean
+    public TomcatServletWebServerFactory tomcatServletWebServerFactory() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory(){
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(connector()); // 添加上面的 connector
+        return tomcat;
+    }
+}
